8d8cf221b9450a4337dfc74be77d947363ef78b9
Reetu Gopi R <reetugopi@Reetus-MacBook-Air.local>
2025-04-18 09:04:01 +0530

    fix: use dynamic port for Render

--- Desktop/rit-chatbot/frontend/backend/server.js
+++ Desktop/rit-chatbot/frontend/backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors()); // allow frontend to talk to backend
 app.use(express.json());
